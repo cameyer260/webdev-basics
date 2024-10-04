d786b5b2a594cfd10249ab4cd86439a9812d12e9
Christopher Meyer <cameyer06@gmail.com>
2024-10-03 23:13:59 -0500

    some work done on navbar and homepage

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', async (request, response) => {
     response.send( await readFile('./home.html', 'utf8'));
 });
